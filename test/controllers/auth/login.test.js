a2573fc856e842ccbe82de58302bc779dcc83caa
Asier Cidon <asier.cidon@gmail.com>
2020-04-20 17:13:15 +0200

    Added authentication test change

--- test/controllers/auth/login.test.js
+++ test/controllers/auth/login.test.js
@@ -8,41 +8,34 @@ const mongoose = require("mongoose");
 const Mockgoose = require("mock-mongoose").Mockgoose;
 const mockgoose = new Mockgoose(mongoose);
 
-before(function(done) {
-    mockgoose.prepareStorage().then(function() {
-        mongoose.connect(
-            "mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}",
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true
-            },
-            function(err) {
-                done(err);
-            }
-        );
-    });
+before(function (done) {
+  mockgoose.prepareStorage().then(function () {
+    mongoose.connect(
+      "mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      },
+      function (err) {
+        done(err);
+      }
+    );
+  });
 });
 
 describe("Authentication Library", () => {
-    it("Should create a user", async () => {
-        assert.equal(await userLib.new(userLogin), true);
+  it("Should create a user", async () => {
+    assert.equal(await userLib.new(userLogin), true);
+  });
+  it("Login with user", async () => {
+    const login = await authLib.login({
+      username: "robot123456789001",
+      password: "robot1234567890./*01",
     });
-    it("Login with user", async () => {
-        const login = await authLib.login({
-            username: "robot123456789001",
-            password: "robot1234567890./*01"
-        });
-        assert.equal(login, "User Logged");
-    });
-    it("Should delete an user", async () => {
-        assert.equal(
-            await userLib.delete({ username: userLogin.username }),
-            true
-        );
-    });
-
-    it("Finish", done => {
-        done();
-    });
+    assert.equal(login, "User Logged");
+  });
+  it("Should delete an user", async () => {
+    assert.equal(await userLib.delete({ username: userLogin.username }), true);
+  });
 });
